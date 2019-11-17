210124a769bc5f8ea33a7b505a4a334a9a63f0f6
GuardianSC <calutz1125@gmail.com>
2019-11-17 13:28:06 -0800

    Initial Commit
    Functional first person player controller and camera, small test area, flickering lights. Next step to implement is trigger that spawns an enemy

---
+++ Assets/Scripts/Environment/Flicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour
+{
+    public float minTime, maxTime;
+    Light thisLight;
+
+    private void Start()
+    {
+        thisLight = GetComponent<Light>();
+        StartCoroutine("LightFlicker");
+    }
+    
+    private IEnumerator LightFlicker()
+    {
+        while (true)
+        {
+            thisLight.enabled = true;
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+            thisLight.enabled = false;
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+        }        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    Camera playerCamera;
+
+    public float xMin = -60f;
+    public float xMax = 60f;
+    public float yMin = -360f;
+    public float yMax = 360f;
+
+    public float xSensitivity = 2f;
+    public float ySensitivity = 2f;
+
+    public float xRotation = 0f;
+    public float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to keep it in the game window
+        playerCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yRotation += Input.GetAxisRaw("Mouse X") * ySensitivity;
+        xRotation += Input.GetAxisRaw("Mouse Y") * xSensitivity;
+
+        // Clamp to keep camera up/down rotation within a reasonable/realistic/wanted range
+        xRotation = Mathf.Clamp(xRotation, xMin, xMax);
+
+        playerCamera.transform.localEulerAngles = new Vector3(-xRotation, yRotation, 0);
+        //Debug.Log("Camera forward" + playerCamera.transform.forward);
+
+        // Unlock the cursor
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None; // Make cursor visible
+            Cursor.visible = true;
+        }
+    }
+}
